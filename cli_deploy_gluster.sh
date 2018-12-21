8371170760e6a94608e608aec73167061f8299d6
tanewill <taylor.newill@oracle.com>
2018-12-21 17:44:11 -0500

    authentication

--- cli_deploy_gluster.sh
+++ cli_deploy_gluster.sh
@@ -37,21 +37,21 @@ create_headnode()
 attach_blocks()
 {
   echo
-  echo 'Adding key to head node'
-  n=0
-  until [ $n -ge 5 ]
-  do
-    scp -o StrictHostKeyChecking=no -i $PRE.key $PRE.key $USER@$masterIP:/home/$USER/.ssh/id_rsa && break
-    n=$[$n+1]
-    sleep 60
-  done
-
   echo 'Waiting for node to complete configuration: 'date +%T
-  ssh -i $PRE.key $USER@$masterIP 'while [ ! -f /var/log/CONFIG_COMPLETE ]; do sleep 30; echo "Waiting for node to complete configuration: `date +%T`"; done'
+  ssh -i $PRE.key $USER@$IP 'while [ ! -f /var/log/CONFIG_COMPLETE ]; do sleep 30; echo "Waiting for node to complete configuration: `date +%T`"; done'
   for i in `seq $server_nodes -1 1`; do
-    echo 'Attaching block volume to head node: 'date +%T' '%D
+    echo -e "${GREEN}Attaching block volume to head node: ${NC}"date +%T' '%D
     IID=`oci compute instance list --compartment-id $compartment_id --region $region | jq -r '.data[] | select(."display-name" | contains ("'$PRE-$i'")) | .id'`
     IP=`oci compute instance list-vnics --region $region --instance-id $IID | jq -r '.data[]."public-ip"'`
+    echo
+    echo -e "${GREEN}Adding key to head node${NC}"
+    n=0
+    until [ $n -ge 5 ]
+    do
+      scp -o StrictHostKeyChecking=no -i $PRE.key $PRE.key $USER@$IP:/home/$USER/.ssh/id_rsa && break
+      n=$[$n+1]
+      sleep 60
+    done  
     for k in `seq 1 $blk_num`; do
       BVID=`oci bv volume list --compartment-id $compartment_id --region $region | jq -r '.data[] | select(."display-name" | contains ("'gluster-block-$PRE-$i-$k'")) | .id'`
       attachID=`oci compute volume-attachment attach --region $region --instance-id $IID --type iscsi --volume-id $BVID --wait-for-state ATTACHED | jq -r '.data.id'`
