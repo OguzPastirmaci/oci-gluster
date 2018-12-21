3c4829f0b3533146b66c9cf78aed9a6e8c27e276
tanewill <taylor.newill@oracle.com>
2018-12-21 11:17:02 -0500

    sync

--- cli_deploy_gluster.sh
+++ cli_deploy_gluster.sh
@@ -11,11 +11,11 @@ create_key()
 create_network()
 {
   #CREATE NETWORK
-  V=`oci network vcn create --region $region --cidr-block 10.0.$subnet.0/24 --compartment-id $compartment_id --display-name "hpc_vcn-$PRE" --wait-for-state AVAILABLE | jq -r '.data.id'`
-  NG=`oci network internet-gateway create --region $region -c $compartment_id --vcn-id $V --is-enabled TRUE --display-name "hpc_ng-$PRE" --wait-for-state AVAILABLE | jq -r '.data.id'`
-  RT=`oci network route-table create --region $region -c $compartment_id --vcn-id $V --display-name "hpc_rt-$PRE" --wait-for-state AVAILABLE --route-rules '[{"cidrBlock":"0.0.0.0/0","networkEntityId":"'$NG'"}]' | jq -r '.data.id'`
-  SL=`oci network security-list create --region $region -c $compartment_id --vcn-id $V --display-name "hpc_sl-$PRE" --wait-for-state AVAILABLE --egress-security-rules '[{"destination":  "0.0.0.0/0",  "protocol": "all", "isStateless":  null}]' --ingress-security-rules '[{"source":  "0.0.0.0/0",  "protocol": "all", "isStateless":  null}]' | jq -r '.data.id'`
-  S=`oci network subnet create -c $compartment_id --vcn-id $V --region $region --availability-domain "$AD" --display-name "hpc_subnet-$PRE" --cidr-block "10.0.$subnet.0/26" --route-table-id $RT --security-list-ids '["'$SL'"]' --wait-for-state AVAILABLE | jq -r '.data.id'`
+  V=`oci network vcn create --region $region --cidr-block 10.0.$subnet.0/24 --compartment-id $compartment_id --display-name "gluster_vcn-$PRE" --wait-for-state AVAILABLE | jq -r '.data.id'`
+  NG=`oci network internet-gateway create --region $region -c $compartment_id --vcn-id $V --is-enabled TRUE --display-name "gluster_ng-$PRE" --wait-for-state AVAILABLE | jq -r '.data.id'`
+  RT=`oci network route-table create --region $region -c $compartment_id --vcn-id $V --display-name "gluster_rt-$PRE" --wait-for-state AVAILABLE --route-rules '[{"cidrBlock":"0.0.0.0/0","networkEntityId":"'$NG'"}]' | jq -r '.data.id'`
+  SL=`oci network security-list create --region $region -c $compartment_id --vcn-id $V --display-name "gluster_sl-$PRE" --wait-for-state AVAILABLE --egress-security-rules '[{"destination":  "0.0.0.0/0",  "protocol": "all", "isStateless":  null}]' --ingress-security-rules '[{"source":  "0.0.0.0/0",  "protocol": "all", "isStateless":  null}]' | jq -r '.data.id'`
+  S=`oci network subnet create -c $compartment_id --vcn-id $V --region $region --availability-domain "$AD" --display-name "gluster_subnet-$PRE" --cidr-block "10.0.$subnet.0/26" --route-table-id $RT --security-list-ids '["'$SL'"]' --wait-for-state AVAILABLE | jq -r '.data.id'`
 }
 
 create_headnode()
@@ -23,9 +23,12 @@ create_headnode()
   #CREATE BLOCK AND HEADNODE
   BLKSIZE_GB=`expr $blksize_tb \* 1024`
   for i in `seq 1 $server_nodes`; do
-    masterID=`oci compute instance launch $INFO --shape "$gluster_server_shape" --display-name "glusterfs-server$i" --image-id $OS --subnet-id $S --private-ip 10.0.$subnet.1$i --wait-for-state RUNNING --user-data-file scripts/gluster_configure.sh --ssh-authorized-keys-file $PRE.key.pub | jq -r '.data.id'`;
+    echo CREATING glusterfs-server$i
+    masterID=`oci compute instance launch $INFO --shape "$gluster_server_shape" --display-name "glusterfs-server-$PRE-$i" --image-id $OS --subnet-id $S --private-ip 10.0.$subnet.1$i --wait-for-state RUNNING --user-data-file scripts/gluster_configure.sh --ssh-authorized-keys-file $PRE.key.pub | jq -r '.data.id'`;
     for k in `seq 1 $blk_num`; do
+      echo CREATING glusterfs-block-$PRE-$i-$k
       BV=`oci bv volume create $INFO --display-name "gluster-block-$PRE-$i-$k" --size-in-gbs $BLKSIZE_GB --wait-for-state AVAILABLE | jq -r '.data.id'`;
+      echo ATTACHING glusterfs-block-$PRE-$i-$k
       attachID=`oci compute volume-attachment attach --region $region --instance-id $masterID --type iscsi --volume-id $BV --wait-for-state ATTACHED | jq -r '.data.id'`;
       attachIQN=`oci compute volume-attachment get --volume-attachment-id $attachID --region $region | jq -r .data.iqn`;
       attachIPV4=`oci compute volume-attachment get --volume-attachment-id $attachID --region $region | jq -r .data.ipv4`;
