5f6b3dd1c59fd9391765f220d1806910122bad77
Taylor Newill <17301351+tanewill@users.noreply.github.com>
2019-01-03 10:56:27 -0500

    Update cli_deploy_gluster.sh

--- cli_deploy_gluster.sh
+++ cli_deploy_gluster.sh
@@ -16,17 +16,17 @@ create_network()
   if [ -z "$vcn_id" ]
   then
     echo Create Network
-    V=`oci network vcn create --region $region --cidr-block $subnet.0/24 --compartment-id $compartment_id --display-name "gluster_vcn-$PRE" --wait-for-state AVAILABLE | jq -r '.data.id'`
-    NG=`oci network internet-gateway create --region $region -c $compartment_id --vcn-id $V --is-enabled TRUE --display-name "gluster_ng-$PRE" --wait-for-state AVAILABLE | jq -r '.data.id'`
-    RT=`oci network route-table create --region $region -c $compartment_id --vcn-id $V --display-name "gluster_rt-$PRE" --wait-for-state AVAILABLE --route-rules '[{"cidrBlock":"0.0.0.0/0","networkEntityId":"'$NG'"}]' | jq -r '.data.id'`
-    SL=`oci network security-list create --region $region -c $compartment_id --vcn-id $V --display-name "gluster_sl-$PRE" --wait-for-state AVAILABLE --egress-security-rules '[{"destination":  "0.0.0.0/0",  "protocol": "all", "isStateless":  null}]' --ingress-security-rules '[{"source":  "0.0.0.0/0",  "protocol": "all", "isStateless":  null}]' | jq -r '.data.id'`
-    S=`oci network subnet create -c $compartment_id --vcn-id $V --region $region --availability-domain "$AD" --display-name "gluster_subnet-$PRE" --cidr-block "$subnet.0/26" --route-table-id $RT --security-list-ids '["'$SL'"]' --wait-for-state AVAILABLE | jq -r '.data.id'`
+    V=`oci network vcn create --profile $profile --region $region --cidr-block $subnet.0/24 --compartment-id $compartment_id --display-name "gluster_vcn-$PRE" --wait-for-state AVAILABLE | jq -r '.data.id'`
+    NG=`oci network internet-gateway create --profile $profile --region $region -c $compartment_id --vcn-id $V --is-enabled TRUE --display-name "gluster_ng-$PRE" --wait-for-state AVAILABLE | jq -r '.data.id'`
+    RT=`oci network route-table create --profile $profile --region $region -c $compartment_id --vcn-id $V --display-name "gluster_rt-$PRE" --wait-for-state AVAILABLE --route-rules '[{"cidrBlock":"0.0.0.0/0","networkEntityId":"'$NG'"}]' | jq -r '.data.id'`
+    SL=`oci network security-list create --profile $profile --region $region -c $compartment_id --vcn-id $V --display-name "gluster_sl-$PRE" --wait-for-state AVAILABLE --egress-security-rules '[{"destination":  "0.0.0.0/0",  "protocol": "all", "isStateless":  null}]' --ingress-security-rules '[{"source":  "0.0.0.0/0",  "protocol": "all", "isStateless":  null}]' | jq -r '.data.id'`
+    S=`oci network subnet create -c $compartment_id --vcn-id $V --profile $profile --region $region --availability-domain "$AD" --display-name "gluster_subnet-$PRE" --cidr-block "$subnet.0/26" --route-table-id $RT --security-list-ids '["'$SL'"]' --wait-for-state AVAILABLE | jq -r '.data.id'`
   else
-    echo Existing Network
+    echo Existing Network Present
     V=$vcn_id
     S=$subnet_id
     NG=`oci network internet-gateway list --compartment-id ocid1.compartment.oc1..aaaaaaaathxc5bc6bv5qgqrzpc2ggnv2lrpcgsvbjpgugoiylw64s3qi6yia --vcn-id ocid1.vcn.oc1.phx.aaaaaaaauuihsosn6ehnjhbhgy5c45yz4r5zypnd4xexujp5ldheyaqk3o2q | jq -r .data[].id`
-    SL=`oci network security-list create --region $region -c $compartment_id --vcn-id $V --display-name "gluster_sl-$PRE" --wait-for-state AVAILABLE --egress-security-rules '[{"destination":  "0.0.0.0/0",  "protocol": "all", "isStateless":  null}]' --ingress-security-rules '[{"source":  "0.0.0.0/0",  "protocol": "all", "isStateless":  null}]' | jq -r '.data.id'`
+    SL=`oci network security-list create --profile $profile --region $region -c $compartment_id --vcn-id $V --display-name "gluster_sl-$PRE" --wait-for-state AVAILABLE --egress-security-rules '[{"destination":  "0.0.0.0/0",  "protocol": "all", "isStateless":  null}]' --ingress-security-rules '[{"source":  "0.0.0.0/0",  "protocol": "all", "isStateless":  null}]' | jq -r '.data.id'`
     subnet=`oci network subnet list -c $compartment_id --vcn-id $V | jq -r '.data[]."virtual-router-ip"' | awk -F. '{print $1"."$2"."$3}'`
   fi
 }
@@ -49,23 +49,23 @@ create_headnode()
 
 configure_storage()
 {
-  IID=`oci compute instance list --compartment-id $compartment_id --region $region | jq -r '.data[] | select(."display-name" | contains ("'$PRE-$i'")) | .id'`
-  IP=`oci compute instance list-vnics --region $region --instance-id $IID | jq -r '.data[]."public-ip"'`
+  IID=`oci compute instance list --compartment-id $compartment_id --profile $profile --region $region | jq -r '.data[] | select(."display-name" | contains ("'$PRE-$i'")) | .id'`
+  IP=`oci compute instance list-vnics --profile $profile --region $region --instance-id $IID | jq -r '.data[]."public-ip"'`
   echo -e "${GREEN}ADDING key to head node${NC}"
 
   for i in `seq $server_nodes -1 1`; do
     n=0
     until [ $n -ge 5 ]; do scp -o StrictHostKeyChecking=no -i $PRE.key $PRE.key $USER@$IP:/home/$USER/.ssh/id_rsa && break; n=$[$n+1]; sleep 30; done
     ssh -i $PRE.key $USER@$IP 'while [ ! -f /var/log/CONFIG_COMPLETE ]; do sleep 30; echo "WAITING for node to complete configuration: `date +%T`"; done'
-    IID=`oci compute instance list --compartment-id $compartment_id --region $region | jq -r '.data[] | select(."display-name" | contains ("'$PRE-$i'")) | .id'`
-    IP=`oci compute instance list-vnics --region $region --instance-id $IID | jq -r '.data[]."public-ip"'`
+    IID=`oci compute instance list --compartment-id $compartment_id --profile $profile --region $region | jq -r '.data[] | select(."display-name" | contains ("'$PRE-$i'")) | .id'`
+    IP=`oci compute instance list-vnics --profile $profile --region $region --instance-id $IID | jq -r '.data[]."public-ip"'`
 
     for k in `seq 1 $blk_num`; do
       echo -e "${GREEN}ATTACHING glusterfs-block-$PRE-$i-$k ${NC}"
-      BVID=`oci bv volume list --compartment-id $compartment_id --region $region | jq -r '.data[] | select(."display-name" | contains ("'gluster-block-$PRE-$i-$k'")) | .id'`
-      attachID=`oci compute volume-attachment attach --region $region --instance-id $IID --type iscsi --volume-id $BVID --wait-for-state ATTACHED | jq -r '.data.id'`
-      attachIQN=`oci compute volume-attachment get --volume-attachment-id $attachID --region $region | jq -r .data.iqn`
-      attachIPV4=`oci compute volume-attachment get --volume-attachment-id $attachID --region $region | jq -r .data.ipv4`
+      BVID=`oci bv volume list --compartment-id $compartment_id --profile $profile --region $region | jq -r '.data[] | select(."display-name" | contains ("'gluster-block-$PRE-$i-$k'")) | .id'`
+      attachID=`oci compute volume-attachment attach --profile $profile --region $region --instance-id $IID --type iscsi --volume-id $BVID --wait-for-state ATTACHED | jq -r '.data.id'`
+      attachIQN=`oci compute volume-attachment get --volume-attachment-id $attachID --profile $profile --region $region | jq -r .data.iqn`
+      attachIPV4=`oci compute volume-attachment get --volume-attachment-id $attachID --profile $profile --region $region | jq -r .data.ipv4`
       ssh -o StrictHostKeyChecking=no -i $PRE.key $USER@$IP sudo sh /root/oci-hpc-ref-arch/scripts/mount_block.sh attach $attachIQN $attachIPV4
     done
     echo -e "${GREEN}CONFIGURING gluster-server-$PRE-$i ${NC}"
@@ -76,7 +76,7 @@ configure_storage()
   sleep 30
   echo $ip_list
   ssh -i $PRE.key $USER@$IP "chmod +x scripts/*.sh; cd /home/$USER/scripts/; pwd; sudo -E bash -c '/home/$USER/scripts/gluster_cifs_configure.sh -v glustervol -m $subnet.11 -n "$ip_list" -b "/bricks/brick1" -u opc -p "password123"'"
-  smbclient -L localhost -U%
+  #smbclient -L localhost -U%
 }
 
 
@@ -102,21 +102,21 @@ EOF
 
 cat << "EOF" >> removeCluster-$PRE.sh
 echo -e "Removing: Gluster Nodes"
-for instanceid in $(oci compute instance list --region $region --compartment-id $compartment_id | jq -r '.data[] | select(."display-name" | contains ("'$PRE'")) | .id'); do oci compute instance terminate --region $region --instance-id $instanceid --force; done
+for instanceid in $(oci compute instance list --profile $profile --region $region --compartment-id $compartment_id | jq -r '.data[] | select(."display-name" | contains ("'$PRE'")) | .id'); do oci compute instance terminate --profile $profile --region $region --instance-id $instanceid --force; done
 sleep 60
 echo -e "Removing: Blocks"
-for id in `oci bv volume list --compartment-id $compartment_id --region $region | jq -r '.data[] | select(."display-name" | contains ("'$PRE'")) | .id'`; do oci bv volume delete --region $region --volume-id $id --force; done
+for id in `oci bv volume list --compartment-id $compartment_id --profile $profile --region $region | jq -r '.data[] | select(."display-name" | contains ("'$PRE'")) | .id'`; do oci bv volume delete --profile $profile --region $region --volume-id $id --force; done
 sleep 60
 echo -e "Removing: Subnet, Route Table, Security List, Gateway, and VCN"
-oci network subnet delete --region $region --subnet-id $S --force
+oci network subnet delete --profile $profile --region $region --subnet-id $S --force
 sleep 10
-oci network route-table delete --region $region --rt-id $RT --force
+oci network route-table delete --profile $profile --region $region --rt-id $RT --force
 sleep 10
-oci network security-list delete --region $region --security-list-id $SL --force
+oci network security-list delete --profile $profile --region $region --security-list-id $SL --force
 sleep 10
-oci network internet-gateway delete --region $region --ig-id $NG --force
+oci network internet-gateway delete --profile $profile --region $region --ig-id $NG --force
 sleep 10
-oci network vcn delete --region $region --vcn-id $V --force
+oci network vcn delete --profile $profile --region $region --vcn-id $V --force
 
 mv removeCluster-$PRE.sh .removeCluster-$PRE.sh
 mv $PRE.key .$PRE.key
